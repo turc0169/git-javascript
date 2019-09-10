8956dd28d3d3add8328f93be9b5e38b2b62c306c
turc0169 <turc0169@algonquinlive.com>
2019-09-10 14:07:31 -0400

    Fixed formula

--- script.js
+++ script.js
@@ -2,12 +2,6 @@ const name = 'Wilfred Turcotte'
 
 console.log('Hello, my name is ' + name)
 
-const number = '5'
+const number = 5
 
-console.log(number)
-const a = '2'
-
-function multiply (number, a) {
-  return number * a
-} // result should be 10
-console.log(number)
+console.log(((number * 2) * 5) / number) - 7)
