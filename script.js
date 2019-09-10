7e96d9caaf402fc74dd98cfd142e15bbf12ac6ad
turc0169 <turc0169@algonquinlive.com>
2019-09-10 14:03:17 -0400

    applied fixes

--- script.js
+++ script.js
@@ -1,13 +1,13 @@
-console.log('Hello, World!')
 const name = 'Wilfred Turcotte'
 
 console.log('Hello, my name is ' + name)
 
-let number = '5'
+const number = '5'
 
 console.log(number)
+const a = '2'
 
-function multiply (number, '2') {
-  return number * '2'
+function multiply (number, a) {
+  return number * a
 } // result should be 10
 console.log(number)
