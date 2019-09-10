c1c132e48b8321282c25543ea0c5f4eb8295b21b
turc0169 <turc0169@algonquinlive.com>
2019-09-10 13:59:28 -0400

    Creates script.js file. Adds steps 7 to 10

---
+++ script.js
@@ -0,0 +1,13 @@
+console.log('Hello, World!')
+const name = 'Wilfred Turcotte'
+
+console.log('Hello, my name is ' + name)
+
+let number = '5'
+
+console.log(number)
+
+function multiply (number, '2') {
+  return number * '2'
+} // result should be 10
+console.log(number)
